6e929d9660dd0e9ab30625c2f5d717d5d1c9a552
saify <sarfaraz.symits@gmail.com>
2025-01-30 21:04:12 +0500

    TASK 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { MapComponent } from './pages/map/map.component';
 import { AboutComponent } from './pages/about/about.component';
 import { TeamComponent } from './pages/team/team.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'map', pathMatch: 'full' },
+    {path: '', component: HomeComponent},
     {path: 'map', component: MapComponent},
     {path: 'about', component: AboutComponent},
     {path: 'team', component: TeamComponent}
